1f9030b0219428fa80c9d566328ca82d58eb907e
zerochae <zerochae@kakao.com>
2024-12-27 09:19:04 +0900

    feat(#master): neovim

--- nvim/configs/vim-dadbod.lua
+++
@@ -1,2 +0,0 @@
-vim.g.completion_matching_strategy_list = { "exact", "substring" }
-vim.g.completion_matching_ignore_case = 1
--- nvim/core/init.lua
+++
@@ -1,3 +0,0 @@
-require "custom.core.auto_cmd"
-require "custom.core.new_cmd"
-require "custom.core.options"
